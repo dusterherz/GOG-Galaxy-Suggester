9dcbe32618bedff78fd99393a04f9717d2474e02
SparrowBrain <qwxmail@gmail.com>
2021-02-18 20:12:15 +0200

    Switched to bolded details item title

--- frontend/src/components/GameDetailsItem/GameDetailsItem.styles.ts
+++ frontend/src/components/GameDetailsItem/GameDetailsItem.styles.ts
@@ -3,6 +3,6 @@ import { makeStyles } from "@material-ui/core";
 export default makeStyles(theme => ({
     title: {
         display: 'inline',
-        textShadow: '3px 3px 2px' + theme.palette.primary.light,
+        fontWeight: 'bold',
     },
 }));

abb686b6de0b3dc2e6bf950198b9a2985453aaa5
SparrowBrain <qwxmail@gmail.com>
2021-03-06 00:31:03 +0200

    Added gogDb tests to ignore dlc and invisible items

---
+++ src/utils/gogDb.integration.test.data.ts
@@ -0,0 +1,99 @@
+export const prisonArchitect = [`
+    INSERT INTO [GameLinks] ([releaseKey],[userId],[gameId]) VALUES (
+    'gog_1441974651',123123123,NULL);
+    `, `INSERT INTO [GameLinks] ([releaseKey],[userId],[gameId]) VALUES (
+    'steam_233450',123123123,NULL);
+
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1441974651',30,NULL,'{"releases":["steam_233450","gog_1441974651","origin_Origin.OFR.50.0002578","humble_prisonarchitect","humble_prisonarchitect_bundle_steam","humble_storefront_prisonarchitect_steam","humble_introversion_weekly_prisonarchitect","humble_storefront_prisonarchitect","humble_prisonarchitect_bundle_row_steam","humble_prisonarchitect_bundle_rucis_latam_cn_kr_steam","nswitch_010029200AB1C000","totalwar_storefront_prisonarchitect_steam","d2d_storefront_prisonarchitect_steam","humble_introversion_weekly_prisonarchitect_steam","humble_prisonarchitect_paradox_row_steam","itch_cf871ec6c3bfd6180386393a3c5f5e17bba57cd1bb18c567ddf2a7c6","itch_XFxSYWlkc2VydmVyXGdcTXkgRHJpdmVcRml0R2lybCBHYW1lc1xQcmlzb24gQXJjaGl0ZWN0IFtGaXRHaXJsIFJlcGFja10=","origin_08e8230c4cf2cd0751707eac3ae0e3b8946940da","origin_8e6e2338d4e524a6bd00f10e9e83d5b13fc9a1e4c3ab3f4469bc01754488930f","paradox_prison_architect_standard","test_233450","test_\/prison-architect-free-download\/","test_prison-architect","xboxone_2102601227","generic_51153517180596753"]}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1441974651',33,123123123,'{"background":"https:\/\/images.gog.com\/ab679758906a2b48a4e3cfda684cf78288aef89a4fbc39331a6e6d89787eb7e9_glx_bg_top_padding_7.webp?namespace=gamesdb","squareIcon":"https:\/\/images.gog.com\/824738dd3534fba4838d9d4920cc0a780c9dea998a5d45f28a93fbb3bacdcaff_glx_square_icon_v2.webp?namespace=gamesdb","verticalCover":"https:\/\/images.gog.com\/824738dd3534fba4838d9d4920cc0a780c9dea998a5d45f28a93fbb3bacdcaff_glx_vertical_cover.webp?namespace=gamesdb"}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1441974651',34,123123123,'{"criticsScore":81.0769,"developers":["Introversion Software","Double Eleven"],"genres":["Strategy","Indie","Simulator"],"publishers":["Introversion Software"],"releaseDate":1444089600,"themes":["Non-fiction","Sandbox","Business"]}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1441974651',35,123123123,'{"title":"Prison Architect"}'); 
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1441974651',101,123123123,'{"criticsScore":81.0769,"developers":["Introversion Software","Double Eleven"],"genres":["Strategy","Indie","Simulator"],"publishers":["Introversion Software"],"releaseDate":1444089600,"themes":["Non-fiction","Sandbox","Business"]}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1441974651',102,123123123,'{"summary":"The game is a top-down 2D construction and management simulation where the player takes control of building and running a prison. The player is responsible for managing various aspects of their prison including building cells and facilities, planning and connecting utilities, hiring and assigning staff, including a warden, guards, workers, and more. The player needs to recruit staff to unlock more aspects of the game. The player is also responsible for the finance, and keeping their inmates content."}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1441974651',103,123123123,'{"title":"Prison Architect"}');
+
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'steam_233450',30,NULL,'{"releases":["steam_233450","gog_1441974651","origin_Origin.OFR.50.0002578","humble_prisonarchitect","humble_prisonarchitect_bundle_steam","humble_storefront_prisonarchitect_steam","humble_introversion_weekly_prisonarchitect","humble_storefront_prisonarchitect","humble_prisonarchitect_bundle_row_steam","humble_prisonarchitect_bundle_rucis_latam_cn_kr_steam","nswitch_010029200AB1C000","totalwar_storefront_prisonarchitect_steam","d2d_storefront_prisonarchitect_steam","humble_introversion_weekly_prisonarchitect_steam","humble_prisonarchitect_paradox_row_steam","itch_cf871ec6c3bfd6180386393a3c5f5e17bba57cd1bb18c567ddf2a7c6","itch_XFxSYWlkc2VydmVyXGdcTXkgRHJpdmVcRml0R2lybCBHYW1lc1xQcmlzb24gQXJjaGl0ZWN0IFtGaXRHaXJsIFJlcGFja10=","origin_08e8230c4cf2cd0751707eac3ae0e3b8946940da","origin_8e6e2338d4e524a6bd00f10e9e83d5b13fc9a1e4c3ab3f4469bc01754488930f","paradox_prison_architect_standard","test_233450","test_\/prison-architect-free-download\/","test_prison-architect","xboxone_2102601227","generic_51153517180596753"]}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'steam_233450',33,123123123,'{"background":"https:\/\/images.gog.com\/ab679758906a2b48a4e3cfda684cf78288aef89a4fbc39331a6e6d89787eb7e9_glx_bg_top_padding_7.webp?namespace=gamesdb","squareIcon":"https:\/\/images.gog.com\/824738dd3534fba4838d9d4920cc0a780c9dea998a5d45f28a93fbb3bacdcaff_glx_square_icon_v2.webp?namespace=gamesdb","verticalCover":"https:\/\/images.gog.com\/824738dd3534fba4838d9d4920cc0a780c9dea998a5d45f28a93fbb3bacdcaff_glx_vertical_cover.webp?namespace=gamesdb"}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'steam_233450',34,123123123,'{"criticsScore":81.0769,"developers":["Introversion Software","Double Eleven"],"genres":["Strategy","Indie","Simulator"],"publishers":["Introversion Software"],"releaseDate":1444089600,"themes":["Non-fiction","Sandbox","Business"]}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'steam_233450',35,123123123,'{"title":"Prison Architect"}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'steam_233450',101,123123123,'{"criticsScore":81.0769,"developers":["Introversion Software","Double Eleven"],"genres":["Strategy","Indie","Simulator"],"publishers":["Introversion Software"],"releaseDate":1444089600,"themes":["Non-fiction","Sandbox","Business"]}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'steam_233450',102,123123123,'{"summary":"The game is a top-down 2D construction and management simulation where the player takes control of building and running a prison. The player is responsible for managing various aspects of their prison including building cells and facilities, planning and connecting utilities, hiring and assigning staff, including a warden, guards, workers, and more. The player needs to recruit staff to unlock more aspects of the game. The player is also responsible for the finance, and keeping their inmates content."}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'steam_233450',103,123123123,'{"title":"Prison Architect"}');
+    `, `
+    INSERT INTO [GameTimes] ([userId],[releaseKey],[minutesInGame]) VALUES (
+    123123123,'gog_1441974651',417);
+    `, `INSERT INTO [GameTimes] ([userId],[releaseKey],[minutesInGame]) VALUES (
+    123123123,'steam_233450',699);
+    `, `
+    INSERT INTO [ReleaseProperties] ([releaseKey],[isDlc],[isVisibleInLibrary],[gameId]) VALUES (
+    'gog_1441974651',0,1,'51153517180596753');
+    `, `INSERT INTO [ReleaseProperties] ([releaseKey],[isDlc],[isVisibleInLibrary],[gameId]) VALUES (
+    'steam_233450',0,1,'51153517180596753');
+    `
+];
+
+export const galaxyBeta_notVisibleInLibrary = [`
+    INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1207667173',30,NULL,'{"releases":["gog_1207667173","generic_51724209887568068"]}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1207667173',101,123123123,'{"criticsScore":null,"developers":[],"genres":[],"publishers":[],"releaseDate":null,"themes":[]}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1207667173',33,123123123,'{"background":null,"squareIcon":"https:\/\/images.gog.com\/648b6038a468885166d889f64fec397a57ce5f490234d600e73097bb85192e30_glx_square_icon_v2.webp?namespace=gamesdb","verticalCover":"https:\/\/images.gog.com\/648b6038a468885166d889f64fec397a57ce5f490234d600e73097bb85192e30_glx_vertical_cover.webp?namespace=gamesdb"}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1207667173',34,123123123,'{"criticsScore":null,"developers":[],"genres":[],"publishers":[],"releaseDate":null,"themes":[]}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1207667173',35,123123123,'{"title":"Galaxy Beta"}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1207667173',102,123123123,'{"summary":""}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1207667173',103,123123123,'{"title":"Galaxy Beta"}');
+
+    `, `INSERT INTO [GameLinks] ([releaseKey],[userId],[gameId]) VALUES (
+    'gog_1207667173',123123123,NULL);
+
+    `, `INSERT INTO [GameTimes] ([userId],[releaseKey],[minutesInGame]) VALUES (
+    123123123,'gog_1207667173',0);
+
+    `, `INSERT INTO [ReleaseProperties] ([releaseKey],[isDlc],[isVisibleInLibrary],[gameId]) VALUES (
+    'gog_1207667173',0,0,'51724209887568068');
+`];
+
+export const cyberpunkGoodies_dlc = [`
+    INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1486144755',30,NULL,'{"releases":["generic_53448971734326669"]}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1486144755',101,123123123,'{"criticsScore":null,"developers":[],"genres":["Role-playing (RPG)"],"publishers":[],"releaseDate":1607558400,"themes":["Open world","Science fiction","Sandbox"]}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1486144755',33,123123123,'{"background":"https:\/\/images.gog.com\/066b628ee36ee7fb7968e3ddfae45b05d8d366b3c1b4dde1ce12f6b9762bbf62_glx_bg_top_padding_7.webp?namespace=gamesdb","squareIcon":"https:\/\/images.gog.com\/cf7b600ce3c03b17a20dd4b4eebc60bcf14eff154a7cad590d39f7493453ac06_glx_square_icon_v2.webp?namespace=gamesdb","verticalCover":"https:\/\/images.gog.com\/cf7b600ce3c03b17a20dd4b4eebc60bcf14eff154a7cad590d39f7493453ac06_glx_vertical_cover.webp?namespace=gamesdb"}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1486144755',34,123123123,'{"criticsScore":null,"developers":[],"genres":["Role-playing (RPG)"],"publishers":[],"releaseDate":1607558400,"themes":["Open world","Science fiction","Sandbox"]}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1486144755',35,123123123,'{"title":"Cyberpunk 2077 Goodies Collection"}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1486144755',102,123123123,'{"summary":"The upcoming RPG from CD Projekt RED based on the Cyberpunk 2020 tabletop RPG created by Mike Pondsmith. \r\n \r\nIn Cyberpunk 2077 you play as V — a hired gun on the rise — and you just got your first serious contract. In a world of cyberenhanced street warriors, tech-savvy netrunners and corporate lifehackers, today you take your first step towards becoming an urban legend."}');
+    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
+    'gog_1486144755',103,123123123,'{"title":"Cyberpunk 2077 Goodies Collection"}');
+
+    `, `INSERT INTO [GameLinks] ([releaseKey],[userId],[gameId]) VALUES (
+    'gog_1486144755',123123123,NULL);
+
+    `, `INSERT INTO [GameTimes] ([userId],[releaseKey],[minutesInGame]) VALUES (
+    123123123,'gog_1486144755',0);
+
+    `, `INSERT INTO [ReleaseProperties] ([releaseKey],[isDlc],[isVisibleInLibrary],[gameId]) VALUES (
+    'gog_1486144755',1,1,'53448971734326669');
+`];
--- src/utils/gogDb.integration.test.ts
+++ src/utils/gogDb.integration.test.ts
@@ -2,6 +2,7 @@ import { SqlJs } from "sql.js/module";
 import initSqlJs from "sql.js";
 import { readGogGames } from "./gogDb";
 import each from 'jest-each';
+import { cyberpunkGoodies_dlc, galaxyBeta_notVisibleInLibrary, prisonArchitect } from "./gogDb.integration.test.data";
 
 let db: SqlJs.Database;
 
@@ -22,14 +23,6 @@ const createGameLinks = () => {
     CREATE UNIQUE INDEX [GameLinks_sqlite_autoindex_GameLinks_1] ON [GameLinks] ([releaseKey] ASC,[userId] ASC);
   `;
   runSql(createTable);
-
-  let addData = [`
-  INSERT INTO [GameLinks] ([releaseKey],[userId],[gameId]) VALUES (
-    'gog_1441974651',123123123,NULL);
-  `, `INSERT INTO [GameLinks] ([releaseKey],[userId],[gameId]) VALUES (
-    'steam_233450',123123123,NULL);
-  `];
-  addData.forEach(runSql);
 }
 
 const createGamePieceTypes = () => {
@@ -103,39 +96,6 @@ const createGamePieces = () => {
     CREATE UNIQUE INDEX [GamePieces_sqlite_autoindex_GamePieces_1] ON [GamePieces] ([releaseKey] ASC,[gamePieceTypeId] ASC,[userId] ASC);
   `;
   runSql(createTable);
-
-  let addData = [`
-    INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
-    'gog_1441974651',30,NULL,'{"releases":["steam_233450","gog_1441974651","origin_Origin.OFR.50.0002578","humble_prisonarchitect","humble_prisonarchitect_bundle_steam","humble_storefront_prisonarchitect_steam","humble_introversion_weekly_prisonarchitect","humble_storefront_prisonarchitect","humble_prisonarchitect_bundle_row_steam","humble_prisonarchitect_bundle_rucis_latam_cn_kr_steam","nswitch_010029200AB1C000","totalwar_storefront_prisonarchitect_steam","d2d_storefront_prisonarchitect_steam","humble_introversion_weekly_prisonarchitect_steam","humble_prisonarchitect_paradox_row_steam","itch_cf871ec6c3bfd6180386393a3c5f5e17bba57cd1bb18c567ddf2a7c6","itch_XFxSYWlkc2VydmVyXGdcTXkgRHJpdmVcRml0R2lybCBHYW1lc1xQcmlzb24gQXJjaGl0ZWN0IFtGaXRHaXJsIFJlcGFja10=","origin_08e8230c4cf2cd0751707eac3ae0e3b8946940da","origin_8e6e2338d4e524a6bd00f10e9e83d5b13fc9a1e4c3ab3f4469bc01754488930f","paradox_prison_architect_standard","test_233450","test_\/prison-architect-free-download\/","test_prison-architect","xboxone_2102601227","generic_51153517180596753"]}');
-    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
-    'gog_1441974651',33,123123123,'{"background":"https:\/\/images.gog.com\/ab679758906a2b48a4e3cfda684cf78288aef89a4fbc39331a6e6d89787eb7e9_glx_bg_top_padding_7.webp?namespace=gamesdb","squareIcon":"https:\/\/images.gog.com\/824738dd3534fba4838d9d4920cc0a780c9dea998a5d45f28a93fbb3bacdcaff_glx_square_icon_v2.webp?namespace=gamesdb","verticalCover":"https:\/\/images.gog.com\/824738dd3534fba4838d9d4920cc0a780c9dea998a5d45f28a93fbb3bacdcaff_glx_vertical_cover.webp?namespace=gamesdb"}');
-    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
-    'gog_1441974651',34,123123123,'{"criticsScore":81.0769,"developers":["Introversion Software","Double Eleven"],"genres":["Strategy","Indie","Simulator"],"publishers":["Introversion Software"],"releaseDate":1444089600,"themes":["Non-fiction","Sandbox","Business"]}');
-    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
-    'gog_1441974651',35,123123123,'{"title":"Prison Architect"}'); 
-    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
-    'gog_1441974651',101,123123123,'{"criticsScore":81.0769,"developers":["Introversion Software","Double Eleven"],"genres":["Strategy","Indie","Simulator"],"publishers":["Introversion Software"],"releaseDate":1444089600,"themes":["Non-fiction","Sandbox","Business"]}');
-    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
-    'gog_1441974651',102,123123123,'{"summary":"The game is a top-down 2D construction and management simulation where the player takes control of building and running a prison. The player is responsible for managing various aspects of their prison including building cells and facilities, planning and connecting utilities, hiring and assigning staff, including a warden, guards, workers, and more. The player needs to recruit staff to unlock more aspects of the game. The player is also responsible for the finance, and keeping their inmates content."}');
-    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
-    'gog_1441974651',103,123123123,'{"title":"Prison Architect"}');
-
-    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
-    'steam_233450',30,NULL,'{"releases":["steam_233450","gog_1441974651","origin_Origin.OFR.50.0002578","humble_prisonarchitect","humble_prisonarchitect_bundle_steam","humble_storefront_prisonarchitect_steam","humble_introversion_weekly_prisonarchitect","humble_storefront_prisonarchitect","humble_prisonarchitect_bundle_row_steam","humble_prisonarchitect_bundle_rucis_latam_cn_kr_steam","nswitch_010029200AB1C000","totalwar_storefront_prisonarchitect_steam","d2d_storefront_prisonarchitect_steam","humble_introversion_weekly_prisonarchitect_steam","humble_prisonarchitect_paradox_row_steam","itch_cf871ec6c3bfd6180386393a3c5f5e17bba57cd1bb18c567ddf2a7c6","itch_XFxSYWlkc2VydmVyXGdcTXkgRHJpdmVcRml0R2lybCBHYW1lc1xQcmlzb24gQXJjaGl0ZWN0IFtGaXRHaXJsIFJlcGFja10=","origin_08e8230c4cf2cd0751707eac3ae0e3b8946940da","origin_8e6e2338d4e524a6bd00f10e9e83d5b13fc9a1e4c3ab3f4469bc01754488930f","paradox_prison_architect_standard","test_233450","test_\/prison-architect-free-download\/","test_prison-architect","xboxone_2102601227","generic_51153517180596753"]}');
-    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
-    'steam_233450',33,123123123,'{"background":"https:\/\/images.gog.com\/ab679758906a2b48a4e3cfda684cf78288aef89a4fbc39331a6e6d89787eb7e9_glx_bg_top_padding_7.webp?namespace=gamesdb","squareIcon":"https:\/\/images.gog.com\/824738dd3534fba4838d9d4920cc0a780c9dea998a5d45f28a93fbb3bacdcaff_glx_square_icon_v2.webp?namespace=gamesdb","verticalCover":"https:\/\/images.gog.com\/824738dd3534fba4838d9d4920cc0a780c9dea998a5d45f28a93fbb3bacdcaff_glx_vertical_cover.webp?namespace=gamesdb"}');
-    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
-    'steam_233450',34,123123123,'{"criticsScore":81.0769,"developers":["Introversion Software","Double Eleven"],"genres":["Strategy","Indie","Simulator"],"publishers":["Introversion Software"],"releaseDate":1444089600,"themes":["Non-fiction","Sandbox","Business"]}');
-    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
-    'steam_233450',35,123123123,'{"title":"Prison Architect"}');
-    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
-    'steam_233450',101,123123123,'{"criticsScore":81.0769,"developers":["Introversion Software","Double Eleven"],"genres":["Strategy","Indie","Simulator"],"publishers":["Introversion Software"],"releaseDate":1444089600,"themes":["Non-fiction","Sandbox","Business"]}');
-    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
-    'steam_233450',102,123123123,'{"summary":"The game is a top-down 2D construction and management simulation where the player takes control of building and running a prison. The player is responsible for managing various aspects of their prison including building cells and facilities, planning and connecting utilities, hiring and assigning staff, including a warden, guards, workers, and more. The player needs to recruit staff to unlock more aspects of the game. The player is also responsible for the finance, and keeping their inmates content."}');
-    `, `INSERT INTO [GamePieces] ([releaseKey],[gamePieceTypeId],[userId],[value]) VALUES (
-    'steam_233450',103,123123123,'{"title":"Prison Architect"}');
-  `];
-  addData.forEach(runSql);
 }
 
 const createGameTimes = () => {
@@ -149,14 +109,6 @@ const createGameTimes = () => {
     CREATE UNIQUE INDEX [GameTimes_sqlite_autoindex_GameTimes_1] ON [GameTimes] ([userId] ASC,[releaseKey] ASC);
   `;
   runSql(createTable);
-
-  let addData = [`
-    INSERT INTO [GameTimes] ([userId],[releaseKey],[minutesInGame]) VALUES (
-    123123123,'gog_1441974651',417);
-    `, `INSERT INTO [GameTimes] ([userId],[releaseKey],[minutesInGame]) VALUES (
-      123123123,'steam_233450',699);
-  `];
-  addData.forEach(runSql);
 }
 
 const createReleaseProperties = () => {
@@ -171,14 +123,6 @@ const createReleaseProperties = () => {
     CREATE UNIQUE INDEX [ReleaseProperties_sqlite_autoindex_ReleaseProperties_1] ON [ReleaseProperties] ([releaseKey] ASC);
   `;
   runSql(createTable);
-
-  let addData = [`
-    INSERT INTO [ReleaseProperties] ([releaseKey],[isDlc],[isVisibleInLibrary],[gameId]) VALUES (
-    'gog_1441974651',0,1,'51153517180596753');
-    `, `INSERT INTO [ReleaseProperties] ([releaseKey],[isDlc],[isVisibleInLibrary],[gameId]) VALUES (
-    'steam_233450',0,1,'51153517180596753');
-  `];
-  addData.forEach(runSql);
 }
 
 describe('gogDb', () => {
@@ -191,6 +135,10 @@ describe('gogDb', () => {
     createGamePieces();
     createGameTimes();
     createReleaseProperties();
+
+    prisonArchitect.forEach(runSql);
+    galaxyBeta_notVisibleInLibrary.forEach(runSql);
+    cyberpunkGoodies_dlc.forEach(runSql);
   });
 
   each([
@@ -207,7 +155,28 @@ describe('gogDb', () => {
     let result = await readGogGames(blob);
 
     expect(result.columns).toContain(column);
-    expect(result.values[0][result.columns.indexOf(column)]).toBe(value);
+    let columnIndex = result.columns.indexOf(column);
+    expect(result.values.some(x => x[columnIndex] === value)).toBeTruthy();
+  });
+
+  it('ignore releases with isVisibleInLibrary=0 in ReleaseProperties', async () => {
+    let data = db.export();
+    const blob = new Blob([data]);
+
+    let result = await readGogGames(blob);
+
+    let releaseKeysIndex = result.columns.indexOf('releaseKeys');
+    result.values.forEach(x => expect(x[releaseKeysIndex] === 'gog_1207667173').toBeFalsy())
+  });
+
+  it('ignore dlc', async () => {
+    let data = db.export();
+    const blob = new Blob([data]);
+
+    let result = await readGogGames(blob);
+
+    let releaseKeysIndex = result.columns.indexOf('releaseKeys');
+    result.values.forEach(x => expect(x[releaseKeysIndex] === 'gog_1486144755').toBeFalsy())
   });
 });
 

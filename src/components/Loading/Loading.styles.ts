7927cfa153dbf2a4dfee14eee21bbfff0db7bee7
SparrowBrain <qwxmail@gmail.com>
2021-03-07 22:12:27 +0200

    Added text to loading

--- src/components/Loading/Loading.styles.ts
+++ src/components/Loading/Loading.styles.ts
@@ -1,13 +1,10 @@
 import { makeStyles } from "@material-ui/core";
 
 export default makeStyles(theme => ({
-    root: {
+    center: {
         position: 'absolute',
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-    },
-    progress: {
-
     },
 }));
--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Box, CircularProgress } from "@material-ui/core";
+import { Box, CircularProgress, Typography } from "@material-ui/core";
 import useStyles from './Loading.styles';
 
 export default () => {
     const classes = useStyles();
     return (
-        <Box className={classes.root}>
+        <Box className={classes.center}>
+            <Typography className={classes.center}>Loading</Typography>
             <CircularProgress color="secondary" size='30vh' />
         </Box>
     );
